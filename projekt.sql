3193f109ecd90649d5d4ad9efb029c06c0ff0d28
Eva Doležalová <evik.dol62@gmail.com>
2024-11-04 10:47:02 +0100

    optimalizace tabulky

--- projekt.sql
+++ projekt.sql
@@ -1,5 +1,5 @@
-CREATE TABLE t_Eva_DOLEZALOVA_project_SQL_primary_final
-SELECT 
+CREATE TABLE t_Eva_Dolezalova_project_SQL_primary_final
+SELECT
   	cp.id,
 	cp.value, 
 	cp.value_type_code AS kod_hodnot_mezd,
@@ -7,28 +7,39 @@ SELECT
 	cp.unit_code AS kod_jednotek,
 	cpu.name AS jednotka,
 	cp.calculation_code AS kod_kalkulaci,
-	cpc.name AS kalkulace,
+	cpc. name AS kalkulace,
 	cp.industry_branch_code AS kod_odvetvi,
 	cpib.name AS odvetvi,
-	cp2.value AS cena,
-	cp2.category_code AS kod_kategorie,
-	cp2.date_from AS datum_od,
-	cp2.date_to AS datum_do,
-	cp2.region_code AS kod_regionu,
-	YEAR (date_from) AS rok,
-	quarter(date_from) AS kvartal,
-	produkt
+	vpr.cena,
+	vpr.kod_kategorie,
+	vpr.datum_od,
+	vpr.datum_do,
+	vpr.kod_regionu,
+	cp.payroll_year,
+	vpr.rok,
+	vpr.kvartal,
+	vpr.produkt,
+	vpr.region
 	FROM czechia_payroll cp
 	LEFT JOIN czechia_payroll_calculation cpc ON cp.calculation_code = cpc.code
 	LEFT JOIN czechia_payroll_industry_branch cpib ON cp.industry_branch_code =cpib.code 
 	LEFT JOIN czechia_payroll_unit cpu ON cp.unit_code =cpu.code 
-	LEFT JOIN czechia_payroll_value_type cpvt  ON cp.value_type_code = cpvt.code
-	LEFT JOIN czechia_price cp2 ON YEAR (date_from) = cp.payroll_year AND quarter(date_from) = cp.payroll_quarter
-	LEFT JOIN v_nazev_regionu_cena ON kod_produktu = cp2.category_code;
-	
+	LEFT JOIN czechia_payroll_value_type cpvt ON cp.value_type_code = cpvt.code
+	LEFT JOIN v_produkt_region vpr ON vpr.rok = cp.payroll_year AND vpr.kvartal = cp.payroll_quarter;
 
-SELECT *
-FROM 
+CREATE OR REPLACE VIEW v_produkt_region AS
+SELECT cp.value AS cena,
+	cp.category_code AS kod_kategorie,
+	cp.date_from AS datum_od,
+	cp.date_to AS datum_do,
+	cp.region_code AS kod_regionu,
+	YEAR (date_from) AS rok, 
+	quarter(date_from)AS kvartal, 
+	cpc.name AS produkt,
+	cr.name AS region
+FROM czechia_price cp 
+LEFT JOIN czechia_price_category cpc ON cp.category_code = cpc.code
+LEFT JOIN czechia_region cr ON cp.region_code = cr.code;
 
-SELECT *
-FROM czechia_price_category cpc 
+
+SELECT 
