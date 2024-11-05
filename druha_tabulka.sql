5ca64ef40032e6ff80feff349717e485762fc815
Eva Doležalová <evik.dol62@gmail.com>
2024-11-05 18:39:31 +0100

    t_eva_dolezalova_project_SQL_secondary_final

--- druha_tabulka.sql
+++ druha_tabulka.sql
@@ -1,23 +1,21 @@
 --Druhá tabulka
 
-SELECT *
+CREATE TABLE t_eva_dolezalova_project_SQL_secondary_final AS
+SELECT
+c.country AS zeme,
+c.avg_height AS prumerna_vyska,
+c.calling_code AS predvolba,
+c.capital_city AS hlavni_mesto,
+c.continent AS kontinent,
+c.currency_name AS mena,
+c.religion AS nabozenstvi,
+c.government_type AS vlada,
+c.population_density AS hustota_obyvatel,
+e.GDP AS HDP,
+e.gini AS gini,
+e.taxes AS dane,
+e.YEAR AS rok
 FROM (SELECT * FROM countries WHERE continent = 'Europe')c
-INNER JOIN economies e ON c.country = e.country 
+INNER JOIN economies e ON c.country = e.country
+WHERE year BETWEEN 2006 AND 2018; 
 
-
-
-SELECT *
-FROM economies e 
-WHERE country LIKE '%Cz%'
-
-
-SELECT *
-FROM demographics d 
-WHERE country LIKE '%Eur%'
-
-SELECT *
-FROM religions r 
-WHERE region LIKE '%Eur%'
-
-SELECT *
-FROM life_expectancy le 
