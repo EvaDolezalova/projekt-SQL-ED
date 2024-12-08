2baf28e59b3701f6e768a6fe14a6269dfd6eaa6a
Eva Doležalová <evik.dol62@gmail.com>
2024-12-08 20:02:17 +0100

    uprava_group_by

--- otazka2.sql
+++ otazka2.sql
@@ -2,7 +2,8 @@
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
 SELECT 
-tedpspf.produkt,
+	tedpspf.produkt,
+	tedpspf.rok,
 	Max(tedpspf.rok) AS posledni_rok,
 	min(tedpspf.rok)AS prvni_rok,
 	Round(avg(tedpspf.cena),2) AS prumer_cena,
@@ -13,4 +14,4 @@ FROM t_eva_dolezalova_project_sql_primary_final tedpspf
 	LEFT JOIN czechia_price_category cpc ON tedpspf.produkt = cpc.name 
 	WHERE tedpspf.produkt IN ('Chléb konzumní kmínový','Mléko polotučné pasterované') 
 	              AND rok IN ('2006', '2018')
-GROUP BY produkt,rok
+GROUP BY produkt, rok, name 
--- otazka4.sql
+++ otazka4.sql
@@ -1,8 +1,7 @@
 
 -- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
--- zatím za bez ohledu na produkt a odvětví
 -- pokud by se jednalo o nárůst, tak nebylo nikde překročeno 10%, pouze mezi rok 2008 a 2009 je rozdíl větší, jak 10%, ale je dán tím, že je výrazný pokles v cenách 
--- zkusit i druhou variantu??, zkusit napříč odvětvími a produkty?
+-- použito view - možnost použít i CTE funkce WITH - lepší pro datové soubory z pohledu místa
 
  CREATE VIEW v_prumerne_ceny_mzdy as
  SELECT round(avg(tedpspf.cena),2)AS prumer_cena,
@@ -12,24 +11,29 @@
  FROM t_eva_dolezalova_project_sql_primary_final tedpspf 
  GROUP BY rok, rok_nasledujici;
  
- CREATE VIEW v_prirustky_cen_mezd AS 
+ CREATE OR replace VIEW v_prirustky_cen_mezd AS 
  SELECT vpcm.rok, 
  		vpcm.rok_nasledujici,
  		vpcm.prumer_mzda,
  		vpcm2.prumer_mzda_nasledujici,
  		vpcm.prumer_cena,
  		vpcm2.prumer_cena_nasledujici,
- Round (avg(((vpcm2.prumer_cena_nasledujici-vpcm.prumer_cena)/vpcm.prumer_cena)*100),2) AS prumerny_rocni_narust_cena,
- Round (avg(((vpcm2.prumer_mzda_nasledujici-vpcm.prumer_mzda)/vpcm.prumer_mzda)*100),2) AS prumerny_rocni_narust_mzdy,
- (Round (avg(((vpcm2.prumer_cena_nasledujici-vpcm.prumer_cena)/vpcm.prumer_cena)*100),2)
-  -Round (avg(((vpcm2.prumer_mzda_nasledujici-vpcm.prumer_mzda)/vpcm.prumer_mzda)*100),2)) AS rozdíl,
+ 		Round (avg(((vpcm2.prumer_cena_nasledujici-vpcm.prumer_cena)/vpcm.prumer_cena)*100),2) AS prumerny_rocni_narust_cena,
+ 		Round (avg(((vpcm2.prumer_mzda_nasledujici-vpcm.prumer_mzda)/vpcm.prumer_mzda)*100),2) AS prumerny_rocni_narust_mzdy,
+ 		(Round (avg(((vpcm2.prumer_cena_nasledujici-vpcm.prumer_cena)/vpcm.prumer_cena)*100),2)
+ 		 -Round (avg(((vpcm2.prumer_mzda_nasledujici-vpcm.prumer_mzda)/vpcm.prumer_mzda)*100),2)) AS rozdíl,
   CASE WHEN (ABS(Round (avg(((vpcm2.prumer_cena_nasledujici-vpcm.prumer_cena)/vpcm.prumer_cena)*100),2)
-  -Round (avg(((vpcm2.prumer_mzda_nasledujici-vpcm.prumer_mzda)/vpcm.prumer_mzda)*100),2)) > 10) then 'vyšší' ELSE 'nižší' END 'vysledek'
+ 			-Round (avg(((vpcm2.prumer_mzda_nasledujici-vpcm.prumer_mzda)/vpcm.prumer_mzda)*100),2)) > 10) 
+ 			then 'vyšší' ELSE 'nižší' END 'vysledek'
        FROM v_prumerne_ceny_mzdy vpcm  
  LEFT JOIN 
  (SELECT prumer_cena AS prumer_cena_nasledujici, rok, rok_nasledujici, prumer_mzda AS prumer_mzda_nasledujici FROM v_prumerne_ceny_mzdy) vpcm2 
  ON vpcm.rok_nasledujici = vpcm2.rok  
- GROUP BY rok,vpcm2.prumer_cena_nasledujici,vpcm2.prumer_mzda_nasledujici;
+ GROUP BY rok,vpcm2.prumer_cena_nasledujici,vpcm2.prumer_mzda_nasledujici, vpcm.rok_nasledujici, vpcm.prumer_mzda,
+ 		  vpcm.prumer_cena;
+ 		
+ SELECT *
+ FROM v_prirustky_cen_mezd vpcm 
 
  
   
