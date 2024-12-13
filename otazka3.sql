c93d178857449c569ee175863f9e335abc314430
Eva Doležalová <evik.dol62@gmail.com>
2024-12-13 18:59:43 +0100

    vraceni_win_funkce

--- otazka3.sql
+++ otazka3.sql
@@ -1,7 +1,7 @@
 
 -- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 
-
+-- 1. způsob výpočtu pro průměr přes produkt
 CREATE VIEW v_prumerna_cena AS
 SELECT produkt, 
 	   rok, 
@@ -20,3 +20,17 @@ SELECT vpc.produkt,
  ORDER BY prumerny_rocni_narust ASC;
  
 
+-- 2. způsob výpočtu pro jednotlivé roky
+WITH cte_prumer_cen AS (
+	SELECT 
+			Round(avg(tedpspf.cena),2) AS prumer_cena, 
+			tedpspf.rok,
+			tedpspf.produkt,	
+			LAG(Round(avg(tedpspf.cena),2)) OVER (PARTITION BY tedpspf.produkt ORDER BY rok) AS prumerna_cena_predchozi_rok,
+		    ((Round(avg(tedpspf.cena), 2)) - LAG(Round(avg(tedpspf.cena), 2)) OVER (PARTITION BY tedpspf.produkt ORDER BY rok))
+                   / LAG(Round(avg(tedpspf.cena), 2)) OVER (PARTITION BY tedpspf.produkt ORDER BY rok) * 100 AS prumerny_rocni_narust
+    FROM t_eva_dolezalova_project_sql_primary_final tedpspf 
+	GROUP BY produkt, rok
+	)
+	SELECT produkt, prumerny_rocni_narust, rok
+	FROM cte_prumer_cen;
